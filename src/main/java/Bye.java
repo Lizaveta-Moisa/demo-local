e1d1c7b5d854750f67c8dd32e42ac88b088e3c5b
Lizavete-Moisa <sliza1882@gmail.com>
2025-05-02 11:34:04 +0300

    Initial commit

---
+++ src/main/java/Bye.java
@@ -0,0 +1,10 @@
+public class Bye {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+        int sum = 0;
+
+        sum = a + b;
+        System.out.println("Сумма a и b равна - " + sum);
+    }
+}
---
+++ src/main/java/Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+    public static void main(String[] args) {
+        int c = 5;
+        int d = 3;
+        int sum = 0;
+
+        sum = c - d;
+        System.out.println("Разница между c и d равна - " + sum);
+    }
+}
